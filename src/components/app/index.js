4bdb397ab430b9469ad4ed6e73909da5ecb06b9f
victorbruce <victorbruce82@gmail.com>
2019-10-17 21:26:49 +0000

    Add react router package

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,9 +1,21 @@
 import React, { Component } from "react";
+import Navbar from "../navbar";
+import "bootstrap/dist/css/bootstrap.css";
+import $ from "popper.js";
+import jquery from "jquery";
+import "bootstrap/dist/js/bootstrap.bundle";
 
 class App extends Component {
   state = {};
   render() {
-    return <h1>React Forms!</h1>;
+    return (
+      <main>
+        <Navbar />
+        <div className="container">
+          <h1>React Forms</h1>
+        </div>
+      </main>
+    );
   }
 }
 
