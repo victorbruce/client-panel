87e136acf01bfc3a910c1653d1f159abcc0eb094
victorbruce <victorbruce82@gmail.com>
2019-10-18 23:50:57 +0000

    Add routing

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
+import { Route, Switch } from 'react-router-dom';
 import Navbar from "../navbar";
-
+import LoginForm from '../loginForm';
+import SignupForm from '../signupForm';
 
 class App extends Component {
   state = {};
@@ -10,6 +12,8 @@ class App extends Component {
         <Navbar />
         <div className="container">
           <h1>React Forms</h1>
+          <Route path="/signup" component={SignupForm} />
+          <Route path="/" component={LoginForm} />
         </div>
       </main>
     );
