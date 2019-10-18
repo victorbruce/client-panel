2e3ce7aaad6ea64db4795f794c136dc1074c6a13
victorbruce <victorbruce82@gmail.com>
2019-10-18 23:44:42 +0000

    Add login form component

---
+++ src/components/loginForm/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class LoginForm extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <h1>Login Page</h1>
+      </div>
+    );
+  }
+}
+
+export default LoginForm;
