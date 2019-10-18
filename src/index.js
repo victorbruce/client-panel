7884f6c31e11431387f7d66a500d1e16ccf5b2df
victorbruce <victorbruce82@gmail.com>
2019-10-18 23:56:58 +0000

    Move boostrap to src index.js file

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./components/app";
+import "bootstrap/dist/css/bootstrap.css";
+import $ from "popper.js";
+import jquery from "jquery";
+import "bootstrap/dist/js/bootstrap.bundle";
 
 ReactDOM.render(
   <BrowserRouter>
