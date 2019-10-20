c373976976f9f0e6c6dbc00cb216d1de0d69a8d3
victorbruce <victorbruce82@gmail.com>
2019-10-20 13:12:19 +0000

    Add form inputs

--- src/components/loginForm/index.js
+++ src/components/loginForm/index.js
@@ -1,11 +1,59 @@
 import React, { Component } from "react";
-
+import { Link } from "react-router-dom";
 class LoginForm extends Component {
-  state = {};
+  state = {
+    account: {
+      email: "",
+      password: ""
+    }
+  };
+
+  handleChange = ({ target: input }) => {
+    const account = { ...this.state.account };
+    account[input.name] = input.value;
+    this.setState({ account });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    console.log("submitted");
+  };
   render() {
+    const { account } = this.state;
     return (
-      <div>
-        <h1>Login Page</h1>
+      <div className="container mt-5">
+        <form onSubmit={this.handleSubmit} className="col-6 offset-3">
+          <p className="lead text-center">Login</p>
+          <div className="form-group">
+            <label htmlFor="email">Email address</label>
+            <input
+              className="form-control"
+              type="email"
+              name="email"
+              id="email"
+              onChange={this.handleChange}
+              value={account.email}
+              placeholder="email"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input
+              className="form-control"
+              type="password"
+              name="password"
+              onChange={this.handleChange}
+              value={account.password}
+              id="password"
+              placeholder="password"
+            />
+          </div>
+          <button className="btn btn-primary block">Login</button>
+          <p className="text-center pt-3">
+            <Link to="signup">Don't have an Account?</Link>
+          </p>
+        </form>
       </div>
     );
   }
