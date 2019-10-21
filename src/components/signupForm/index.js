d75bdd23278dc7dcd53dd6131bad74bff8b81cc4
victorbruce <victorbruce82@gmail.com>
2019-10-21 23:56:44 +0000

    Add sign up form and implement validation

--- src/components/signupForm/index.js
+++ src/components/signupForm/index.js
@@ -1,11 +1,39 @@
-import React, { Component } from "react";
+import React from "react";
+import Joi from "joi-browser";
+import Form from "../common/form";
 
-class SignupForm extends Component {
-  state = {};
+class SignupForm extends Form {
+  state = {
+    data: {
+      fullName: "",
+      email: "",
+      password: ""
+    },
+    errors: {}
+  };
+
+  schema = {
+    name: Joi.string().required(),
+    email: Joi.string().email({ minDomainAtoms: 2 }),
+    password: Joi.string()
+      .min(5)
+      .required()
+  };
+
+  doSubmit() {
+    console.log("Registration submitted");
+  }
+
   render() {
     return (
-      <div>
-        <h1>Sign up form page</h1>
+      <div className="container">
+        <form onSubmit={this.handleSubmit} className="col-6 offset-3">
+          <p className="lead">Sign Up</p>
+          {this.renderInput("name", "Name")}
+          {this.renderInput("email", "Email", "email")}
+          {this.renderInput("password", "Password", "password")}
+          {this.renderButton("Sign up")}
+        </form>
       </div>
     );
   }
