fd6a75a80e99e4987a42059ee55ef0c1a04d2d94
victorbruce <victorbruce82@gmail.com>
2020-07-07 22:55:10 +0000

    update: add clients and sidebar component to dashboard

--- src/containers/App/Dashboard/index.tsx
+++ src/containers/App/Dashboard/index.tsx
@@ -1,11 +1,21 @@
 import React from "react";
+
 import AppLayout from "../../../layouts/AppLayout";
+import Clients from "../../../containers/App/Clients";
+import Sidebar from "../../../containers/App/Sidebar";
 
 const Dashboard = () => {
   return (
     <AppLayout>
       <div className="container">
-        <h1>Dashboard</h1>
+        <div className="row">
+          <div className="col-md-10">
+            <Clients />
+          </div>
+          <div className="col-md-2">
+            <Sidebar />
+          </div>
+        </div>
       </div>
     </AppLayout>
   );
