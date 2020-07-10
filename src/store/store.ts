96f904f3cd749726823238ab1719c68e1b8d81f8
victorbruce <victorbruce82@gmail.com>
2020-07-10 14:30:33 +0000

    refactor: store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,20 +1,7 @@
-import { createStore, combineReducers, compose } from "redux";
-import firebase from "firebase/app";
-import "firebase/firestore";
-import { firebaseReducer } from "react-redux-firebase";
-import { createFirestoreInstance, firestoreReducer } from "redux-firestore";
-
-// firebase configuration
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
-};
+import { createStore, compose } from "redux";
+import firebase from '../shared/fbConfig';
+import { createFirestoreInstance } from "redux-firestore";
+import rootReducer from "./reducers";
 
 // react-redux-firebase configuration
 const rrfConfig = {
@@ -22,19 +9,12 @@ const rrfConfig = {
   useFirestoreForProfile: true,
 };
 
-// Initialize firebase instance
-firebase.initializeApp(firebaseConfig);
-
 const composeEnhancers =
   (typeof window === "object" &&
   (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
     ? (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
     : null) || compose;
 
-const rootReducer = combineReducers({
-  firebase: firebaseReducer,
-  firestore: firestoreReducer,
-});
 
 const initialState = {};
 
