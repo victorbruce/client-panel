10f374ee40a5a0f04e672a14ee1c195215cd517a
victorbruce <victorbruce82@gmail.com>
2020-07-11 10:05:31 +0000

    feat: logout implemented

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,41 +1,88 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { compose } from "redux";
+import { firebaseConnect } from "react-redux-firebase";
 
-const Navbar = () => {
-  return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary mb-4">
-      <div className="container">
-        <Link className="navbar-brand" to="/">
-          ClientPanel
-        </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <Link
-                className="nav-link"
-                to="/"
-                tab-index="-1"
-                aria-disabled="true"
-              >
-                Dashboard
-              </Link>
-            </li>
-          </ul>
+class Navbar extends Component<any, any> {
+  state = {
+    isAuthenticated: false,
+  };
+
+  handleLogout = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    e.preventDefault();
+
+    const { firebase } = this.props;
+
+    firebase.logout();
+  };
+
+  static getDerivedStateFromProps = (props, state) => {
+    const { auth } = props;
+
+    if (auth.uid) {
+      return { isAuthenticated: true };
+    } else {
+      return { isAuthenticated: false };
+    }
+  };
+
+  render() {
+    const { auth } = this.props;
+    return (
+      <nav className="navbar navbar-expand-lg navbar-dark bg-primary mb-4">
+        <div className="container">
+          <Link className="navbar-brand" to="/">
+            ClientPanel
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link
+                  className="nav-link"
+                  to="/"
+                  tab-index="-1"
+                  aria-disabled="true"
+                >
+                  Dashboard
+                </Link>
+              </li>
+            </ul>
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <a href="#!" className="nav-link">
+                  {auth && auth.email}
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="#!" className="nav-link" onClick={this.handleLogout}>
+                  Logout
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
-  );
-};
+      </nav>
+    );
+  }
+}
 
-export default Navbar;
+const mapStateToProps = (state) => ({
+  auth: state.firebase.auth,
+});
+
+export default compose<any>(
+  firebaseConnect(),
+  connect(mapStateToProps)
+)(Navbar);
