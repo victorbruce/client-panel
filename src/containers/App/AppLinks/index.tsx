7b9dc2ce17d3197752d1cf96212f0cc0c09728cd
victorbruce <victorbruce82@gmail.com>
2020-07-11 21:47:31 +0000

    update: route protection

--- src/containers/App/AppLinks/index.tsx
+++ src/containers/App/AppLinks/index.tsx
@@ -1,5 +1,9 @@
 import React, { lazy, Suspense } from "react";
 import { Switch, Route } from "react-router-dom";
+import {
+  UserIsAuthenticated,
+  UserIsNotAuthenticated,
+} from "../../../shared/auth";
 
 const Dashboard = lazy(() => import("../Dashboard"));
 const AddClient = lazy(() => import("../AddClient"));
@@ -12,11 +16,27 @@ const AppLinks = () => {
     <div>
       <Suspense fallback={<h1>Loading...</h1>}>
         <Switch>
-          <Route path="/" exact component={Dashboard} />
-          <Route path="/client/add" exact component={AddClient} />
-          <Route path="/client/edit/:id" exact component={EditClient} />
-          <Route path="/client/:id" exact component={ClientDetails} />
-          <Route path="/login" exact component={Login} />
+          <Route path="/" exact component={UserIsAuthenticated(Dashboard)} />
+          <Route
+            path="/client/add"
+            exact
+            component={UserIsAuthenticated(AddClient)}
+          />
+          <Route
+            path="/client/edit/:id"
+            exact
+            component={UserIsAuthenticated(EditClient)}
+          />
+          <Route
+            path="/client/:id"
+            exact
+            component={UserIsAuthenticated(ClientDetails)}
+          />
+          <Route
+            path="/login"
+            exact
+            component={UserIsNotAuthenticated(Login)}
+          />
         </Switch>
       </Suspense>
     </div>
